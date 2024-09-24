d048bae3e0ad1ae713d91d68c42e2337515a08b5
Rolf M <rolfie@outlook.dk>
2024-09-24 11:19:22 +0200

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,28 @@
-import { useState } from 'react';
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 import * as contentful from "contentful";
+import Footer from "./components/footer/Footer";
 
 function App() {
+	const client = contentful.createClient({
+		space: "0es9dhvy2g8b",
+		environment: "master", // defaults to 'master' if not set
+		accessToken: "6izYEm6gSNbyeheG8JBrqct5U1tuE-HpFD-HbN7kD9I",
+	});
 
+	client
+		.getEntries({
+			content_type: "Gallery",
+		})
+		.then((entry) => console.log(entry))
+		.catch(console.error);
 
-  const client = contentful.createClient({
-    space: '0es9dhvy2g8b',
-    environment: 'master', // defaults to 'master' if not set
-    accessToken: '6izYEm6gSNbyeheG8JBrqct5U1tuE-HpFD-HbN7kD9I'
-  })
-  
-  client.getEntries({
-    content_type: "Gallery"
-  })
-    .then((entry) => console.log(entry))
-    .catch(console.error)
-
-  return (
-    <>
-      
-    </>
-  )
+	return (
+		<>
+			
+			<Footer />
+		</>
+	);
 }
 
-export default App
+export default App;
